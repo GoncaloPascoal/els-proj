4c2443dc3d548aa1ebe3b9217edea681bce13096
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-11-02 18:45:21 +0000

    Add XPath support to XML adapter

--- src/pt/up/fe/els2022/adapters/XmlAdapter.java
+++ src/pt/up/fe/els2022/adapters/XmlAdapter.java
@@ -1,18 +1,26 @@
 package pt.up.fe.els2022.adapters;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.collections4.map.ListOrderedMap;
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import pt.up.fe.els2022.model.Table;
-import pt.up.fe.specs.util.xml.XmlDocument;
-import pt.up.fe.specs.util.xml.XmlElement;
-import pt.up.fe.specs.util.xml.XmlNode;
 
 public class XmlAdapter extends StructuredAdapter {
     public XmlAdapter(String path, List<String> columns) {
@@ -21,34 +29,68 @@ public class XmlAdapter extends StructuredAdapter {
 
     @Override
     public Table extractTable(List<File> files) {
+        DocumentBuilder builder;
+        try {
+            builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        }
+        catch (ParserConfigurationException ex) {
+            throw new RuntimeException(ex);
+        }
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        Document document;
+        Node node;
+
         Table table = new Table();
         Map<String, List<String>> rows = new ListOrderedMap<>();
 
-        XmlDocument document;
-        XmlElement element;
-
         for (File file : files) {
-            document = XmlDocument.newInstance(file);
-            element = document.getElementByName(path);
+            try {
+                document = builder.parse(file);
+                node = (Node) xPath.compile(path).evaluate(document, XPathConstants.NODE);
+            }
+            catch (IOException | SAXException | XPathExpressionException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            if (node == null) {
+                throw new RuntimeException("Node specified by path does not exist.");
+            }
 
             if (columns.isEmpty()) {
                 // Get all nodes under element
-                for (XmlNode child : element.getChildren().stream()
-                        .filter(n -> n.getNode().getNodeType() == Node.ELEMENT_NODE)
-                        .collect(Collectors.toList())) {
-                    Node childNode = child.getNode();
-                    String nodeName = childNode.getNodeName();
-                    rows.putIfAbsent(nodeName, new ArrayList<>());
-                    rows.get(nodeName).add(childNode.getTextContent());
+                NodeList children = node.getChildNodes();
+
+                for (int i = 0; i < children.getLength(); ++i) {
+                    Node child = children.item(i);
+
+                    if (child.getNodeType() == Node.ELEMENT_NODE) {
+                        String name = child.getNodeName();
+                        
+                        if (child.getFirstChild().getNodeType() == Node.TEXT_NODE) {
+                            rows.putIfAbsent(name, new ArrayList<>());
+                            rows.get(name).add(child.getTextContent());
+                        }
+                    }
                 }
             }
             else {
                 // Get specific nodes in a particular order
                 for (String column : columns) {
-                    Node childNode = element.getElementByName(column).getNode();
-                    String nodeName = childNode.getNodeName();
-                    rows.putIfAbsent(nodeName, new ArrayList<>());
-                    rows.get(nodeName).add(childNode.getTextContent());
+                    NodeList children = node.getChildNodes();
+
+                    for (int i = 0; i < children.getLength(); ++i) {
+                        Node child = children.item(i);
+                        
+                        if (child.getNodeType() == Node.ELEMENT_NODE) {
+                            String name = child.getNodeName();
+
+                            if (child.getFirstChild().getNodeType() == Node.TEXT_NODE &&
+                                    name == column) {
+                                rows.putIfAbsent(name, new ArrayList<>());
+                                rows.get(name).add(child.getTextContent());
+                            }
+                        }
+                    }
                 }
             }
         }
